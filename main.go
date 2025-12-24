cb0ca5580ff6f09260c51a00924c05d2e1846e8f
patledor <ewebledor@gmail.com>
2025-12-24 14:10:57 +0800

    Fix formatting issues in main.go

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
     "log"
-    "net/http"
+
     "strings"
 
     "github.com/gin-gonic/gin"
@@ -25,3 +25,4 @@ func main() {
     log.Println("Wildcard reverse proxy running on http://localhost:" + port)
     r.Run(":" + port)
 }
+
